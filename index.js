d57e527d84d4361e10a61771767e90852ab8f536
Katelyn Findlay <kfindlay@bluebeam.com>
2015-12-02 12:56:54 -0800

    adding route

--- index.js
+++ index.js
@@ -11,3 +11,7 @@ var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
 	console.log("Listening on " + port);
 });
+
+app.get(‘/spotify-playlist’, function (req, res) {
+	res.redirect('https://open.spotify.com/user/122042988/playlist/5gcDjm36USOXq4b9JWJEFa');
+});
